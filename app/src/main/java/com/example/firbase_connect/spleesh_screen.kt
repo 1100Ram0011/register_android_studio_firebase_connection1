56abbff0f00752ccdb698ad124d286f5c29850ea
rambachan <rambachanyadav558@gmail.com>
2023-10-07 08:19:15 +0530

    start

---
+++ app/src/main/java/com/example/firbase_connect/spleesh_screen.kt
@@ -0,0 +1,11 @@
+package com.example.firbase_connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class spleesh_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spleesh_screen)
+    }
+}
